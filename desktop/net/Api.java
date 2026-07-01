2ba7863a6ac368d52474497bb63cb013ffa4565d
nosey-dewdrop <damummyphus@gmail.com>
2026-07-01 14:53:39 +0300

    add client api facade wrapping all server actions

---
+++ desktop/net/Api.java
@@ -0,0 +1,230 @@
+package net;
+
+import java.util.ArrayList;
+
+import com.google.gson.JsonObject;
+
+import model.Comment;
+import model.Community;
+import model.MbtiResult;
+import model.Message;
+import model.Post;
+import model.User;
+import protocol.Json;
+import protocol.Params;
+
+/*
+ * The friendly face the screens use. Instead of a screen building json and
+ * calling the socket itself, it calls a method here like api.login(...) and
+ * gets back a real object. Under the hood every method turns into a request to
+ * the server and parses the reply.
+ *
+ * If the server says no, ServerClient throws IllegalArgumentException with the
+ * message, which the screens already know how to show.
+ */
+public class Api {
+
+    private static Api instance = new Api();
+
+    public static Api get() {
+        return instance;
+    }
+
+    private ServerClient client = ServerClient.getInstance();
+
+    // ---- small result holders ----
+
+    public static class RegisterResult {
+        public String username;
+        public String email;
+        public boolean emailed;
+        public String code;   // only set when email wasn't sent
+    }
+
+    public static class ResendResult {
+        public boolean emailed;
+        public String code;
+    }
+
+    // ---- auth ----
+
+    public RegisterResult register(String name, String username, String email, String password) {
+        String data = client.send("register", Params.of()
+                .put("displayName", name).put("username", username)
+                .put("email", email).put("password", password).json());
+        return Json.fromJson(data, RegisterResult.class);
+    }
+
+    public User verify(String username, String code) {
+        String data = client.send("verify", Params.of()
+                .put("username", username).put("code", code).json());
+        return Json.fromJson(data, User.class);
+    }
+
+    public ResendResult resend(String username, String email) {
+        String data = client.send("resend", Params.of()
+                .put("username", username).put("email", email).json());
+        return Json.fromJson(data, ResendResult.class);
+    }
+
+    public User login(String usernameOrEmail, String password) {
+        String data = client.send("login", Params.of()
+                .put("usernameOrEmail", usernameOrEmail).put("password", password).json());
+        return Json.fromJson(data, User.class);
+    }
+
+    // ---- profile / onboarding ----
+
+    public void setInterests(String username, ArrayList<String> interests) {
+        client.send("setInterests", Params.of()
+                .put("username", username).putList("interests", interests).json());
+    }
+
+    public MbtiResult submitMbti(String username, int[] answers) {
+        String data = client.send("submitMbti", Params.of()
+                .put("username", username).putInts("answers", answers).json());
+        return Json.fromJson(data, MbtiResult.class);
+    }
+
+    public User getUser(String username) {
+        String data = client.send("getUser", Params.of().put("username", username).json());
+        return Json.fromJson(data, User.class);
+    }
+
+    // ---- communities ----
+
+    public ArrayList<Community> listCommunities() {
+        return toList(client.send("communities.list", Params.of().json()));
+    }
+
+    public Community getCommunity(int id) {
+        String data = client.send("communities.get", Params.of().put("id", id).json());
+        return Json.fromJson(data, Community.class);
+    }
+
+    public ArrayList<Community> byCategory(String category) {
+        return toList(client.send("communities.byCategory", Params.of().put("category", category).json()));
+    }
+
+    public ArrayList<Community> search(String text) {
+        return toList(client.send("communities.search", Params.of().put("text", text).json()));
+    }
+
+    public ArrayList<Community> joined(String username) {
+        return toList(client.send("communities.joined", Params.of().put("username", username).json()));
+    }
+
+    public boolean isMember(String username, int communityId) {
+        String data = client.send("communities.isMember", Params.of()
+                .put("username", username).put("communityId", communityId).json());
+        JsonObject obj = Json.parse(data);
+        return obj.get("member").getAsBoolean();
+    }
+
+    public void join(String username, int communityId) {
+        client.send("communities.join", Params.of()
+                .put("username", username).put("communityId", communityId).json());
+    }
+
+    public void leave(String username, int communityId) {
+        client.send("communities.leave", Params.of()
+                .put("username", username).put("communityId", communityId).json());
+    }
+
+    public ArrayList<Community> homeMatches(String username) {
+        return toList(client.send("communities.homeMatches", Params.of().put("username", username).json()));
+    }
+
+    public Community scoreOne(String username, int communityId) {
+        String data = client.send("communities.scoreOne", Params.of()
+                .put("username", username).put("communityId", communityId).json());
+        return Json.fromJson(data, Community.class);
+    }
+
+    // ---- forum ----
+
+    public ArrayList<Post> posts(int communityId) {
+        String data = client.send("forum.posts", Params.of().put("communityId", communityId).json());
+        Post[] arr = Json.fromJson(data, Post[].class);
+        return arrToList(arr);
+    }
+
+    public Post createPost(int communityId, String author, String title, String body) {
+        String data = client.send("forum.createPost", Params.of()
+                .put("communityId", communityId).put("author", author)
+                .put("title", title).put("body", body).json());
+        return Json.fromJson(data, Post.class);
+    }
+
+    public ArrayList<Comment> comments(int postId) {
+        String data = client.send("forum.comments", Params.of().put("postId", postId).json());
+        Comment[] arr = Json.fromJson(data, Comment[].class);
+        ArrayList<Comment> list = new ArrayList<Comment>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+
+    public void addComment(int postId, String author, String body, int parentId) {
+        client.send("forum.addComment", Params.of()
+                .put("postId", postId).put("author", author)
+                .put("body", body).put("parentId", parentId).json());
+    }
+
+    // ---- messages ----
+
+    public ArrayList<String> partners(String username) {
+        String data = client.send("messages.partners", Params.of().put("username", username).json());
+        String[] arr = Json.fromJson(data, String[].class);
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+
+    public ArrayList<Message> conversation(String me, String other) {
+        String data = client.send("messages.conversation", Params.of()
+                .put("me", me).put("other", other).json());
+        Message[] arr = Json.fromJson(data, Message[].class);
+        ArrayList<Message> list = new ArrayList<Message>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+
+    public void sendMessage(String sender, String receiver, String body) {
+        client.send("messages.send", Params.of()
+                .put("sender", sender).put("receiver", receiver).put("body", body).json());
+    }
+
+    public User findUser(String username) {
+        String data = client.send("messages.findUser", Params.of().put("username", username).json());
+        return Json.fromJson(data, User.class);
+    }
+
+    // ---- helpers ----
+
+    private ArrayList<Community> toList(String data) {
+        Community[] arr = Json.fromJson(data, Community[].class);
+        return arrToList(arr);
+    }
+
+    private ArrayList<Post> arrToList(Post[] arr) {
+        ArrayList<Post> list = new ArrayList<Post>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+
+    private ArrayList<Community> arrToList(Community[] arr) {
+        ArrayList<Community> list = new ArrayList<Community>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+}
--- desktop/protocol/Params.java
+++ desktop/protocol/Params.java
@@ -1,6 +1,9 @@
 package protocol;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
 
 /*
  * A tiny builder so we can put together the data part of a request without
@@ -23,6 +26,26 @@ public class Params {
 
     public Params put(String key, int value) {
         obj.addProperty(key, value);
+        return this;
+    }
+
+    // a list of strings, e.g. the picked interests
+    public Params putList(String key, ArrayList<String> values) {
+        JsonArray arr = new JsonArray();
+        for (int i = 0; i < values.size(); i++) {
+            arr.add(values.get(i));
+        }
+        obj.add(key, arr);
+        return this;
+    }
+
+    // an int array, e.g. the mbti answers
+    public Params putInts(String key, int[] values) {
+        JsonArray arr = new JsonArray();
+        for (int i = 0; i < values.length; i++) {
+            arr.add(values[i]);
+        }
+        obj.add(key, arr);
         return this;
     }
 
